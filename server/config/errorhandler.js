14eac4ea53869aa8c656fd14f75c3c0a933abafc
Juan Macías Opinno <juan.macias@opinno.com>
2018-03-15 14:26:07 +0100

    Feat: Changed all configuration to ES6

--- server/config/errorhandler.js
+++ server/config/errorhandler.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  app.use(function (req, res, next) {
+  app.use((req, res, next) => {
     const error = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
-  app.use(function (error, req, res, next) {
+  app.use((error, req, res, next) => {
     res.locals.message = error.message;
     res.locals.error = req.app.get('env') === 'development' ? error : {};
 
