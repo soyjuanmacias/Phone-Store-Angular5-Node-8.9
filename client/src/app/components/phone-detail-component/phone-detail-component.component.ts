2d9d727eb2c9d3cd8a3568d8591d324f7e9c6641
Juan Macías Opinno <juan.macias@opinno.com>
2018-03-17 11:22:02 +0100

    Created phone components with local mocks

---
+++ client/src/app/components/phone-detail-component/phone-detail-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Phone } from '../../shared/model/phone.model';
+
+@Component({
+  selector: 'app-phone-detail-component',
+  templateUrl: './phone-detail-component.component.html',
+  styleUrls: ['./phone-detail-component.component.css']
+})
+export class PhoneDetailComponentComponent implements OnInit {
+  @Input() phone: Phone = this.phone;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/shared/model/phone.model.ts
@@ -0,0 +1,11 @@
+export class Phone {
+  id: number;
+  description: String;
+  brand: String;
+  model: String;
+  image: String;
+  color: String;
+  price: any;
+  battery: any;
+  screen: any;
+}
